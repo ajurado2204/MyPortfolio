38bd2d549dea5e8f7f7a6f893fc43ef89c1c0568
Alejandra Jurado <ajurado2204@gmail.com>
2016-02-05 21:46:12 -0500

    fixed a side bar error

--- js/forMain.js
+++ js/forMain.js
@@ -2,6 +2,5 @@
 $(".button-collapse").sideNav({
     menuWidth: 200, // Default is 240
     edge: 'left', // Choose the horizontal origin
-    closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
   }
 );
