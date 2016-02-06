e6b136db1aaecb158d46f6034d0acdb30e902d4a
Alejandra Jurado <ajurado2204@gmail.com>
2016-02-05 22:01:00 -0500

    moved files

--- js/forMain.js
+++ js/forMain.js
@@ -1,6 +1,6 @@
 // Initialize collapse button
 $(".button-collapse").sideNav({
     menuWidth: 200, // Default is 240
-    edge: 'left', // Choose the horizontal origin
+    edge: 'left' // Choose the horizontal origin
   }
 );
