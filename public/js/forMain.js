c0f1cbcd9142bfeb58b46f4ab10b9b5f586f37ae
Alejandra Jurado <ajurado2204@gmail.com>
2016-02-07 17:29:23 -0500

    created dynamic routes and used middlewar to handle session

--- public/js/forMain.js
+++ public/js/forMain.js
@@ -14,11 +14,3 @@ $('.contactmodaltrigger').on('click', function(){
   });
 });
 
-$('.loginmodaltrigger').on('click', function(){
-  $('#modal2').openModal({
-    dismissible: false, // Modal can be dismissed by clicking outside of the modal
-    opacity: .3, // Opacity of modal background
-    in_duration: 500, // Transition in duration
-    out_duration: 300 // Transition out duration
-  });
-});
--- server.js
+++ server.js
@@ -2,15 +2,63 @@
  * Created by Ale on 2/7/16.
  */
 var express = require('express');
+var GitHubApi = require('node-github')
+var bodyparser = require('body-parser');
+var session = require('express-session');
+
 var app = express();
 var PORT = process.env.PORT || 8080;
+
+
+app.use(session({
+    secret: 'crazy secret boom!!',
+    cookie: {
+      maxAge: 60000
+    },
+    saveUninitialized:true,
+    resave: false
+  }
+));
+
+app.use(bodyparser.urlencoded({extended:false}));
+
+
+function middleware(req,res,next){
+  var sess = req.session;
+
+  if(sess.authenticated === undefined || sess.authenticated === false){
+    res.redirect("/");
+  }
+
+  next();
+}
 
 app.use('/js', express.static("public/js"));
 app.use('/css', express.static("public/css"));
 app.use('/images', express.static("public/images"));
 
+app.post('/login', function(req,res){
+
+  console.log("HELLO");
+  if(req.body.email.length === 0 || req.body.password.length === 0) {
+    req.session.authenticated = false;
+    res.redirect("/");
+  } else if(req.body.email.length === 0 && req.body.password.length === 0) {
+    req.session.authenticated = false;
+    res.redirect("/");
+  } else {
+    req.session.authenticated = true;
+    res.redirect("/dashboard");
+  }
+
+});
+
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + "/views/main.html");
+});
+
+app.get('/login', function(req,res){
+  res.sendFile(process.cwd() + "/views/login.html");
 });
 
 app.get('/myprojects', function(req, res){
@@ -21,7 +69,26 @@ app.get('/mygithub', function(req, res){
   res.sendFile(process.cwd() + "/views/mygithub.html");
 });
 
+app.get('/dashboard', middleware, function(req,res){
+  res.sendFile(process.cwd() + "/views/dashboard.html")
+});
 
+app.get("/logout", middleware, function(req, res) {
+  req.session.authenticated = false;
+  res.redirect("/");
+});
+
+app.get('/dashboard/:githubuname', function(req, res) {
+  var github = new GitHubApi({
+    version: "3.0.0"
+  });
+
+  github.user.getFrom({
+    user: req.params.githubuname
+  }, function(err, gitResponse){
+    res.send(JSON.stringify(gitResponse));
+  });
+});
 
 app.listen(PORT, function(){
   console.log("listening on port %s", PORT);
