fd8c5b9216fc4c0b475b271113a9bd01a53b96e7
Alejandra Jurado <ajurado2204@gmail.com>
2016-05-09 20:53:27 -0400

    added button event

--- public/js/events.js
+++ public/js/events.js
@@ -4,8 +4,21 @@
 $(document).ready(function(){
   $('.parallax').parallax();
 
-  $(".button-collapse").sideNav();
+  $(".button-collapse").sideNav({
+    menuWidth: 200, // Default is 240
+    edge: 'right', // Choose the horizontal origin
+    closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  });
 
-  Sticker.init('.sticker');
+
+  $(".button").on("click", function( e ) {
+
+    e.preventDefault();
+
+    $("body, html").animate({
+      scrollTop: $( $(this).attr('href') ).offset().top
+    }, 2000);
+
+  });
 
 });
