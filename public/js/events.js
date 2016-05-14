20e3d79a3325769fd4b049aa90976d50a618205f
Alejandra Jurado <ajurado2204@gmail.com>
2016-05-14 01:00:15 -0400

    added event to buttons and text animation

--- public/js/events.js
+++ public/js/events.js
@@ -2,12 +2,26 @@
  * Created by Ale on 5/3/16.
  */
 $(document).ready(function(){
+
   $('.parallax').parallax();
 
   $(".button-collapse").sideNav({
     menuWidth: 200, // Default is 240
     edge: 'right', // Choose the horizontal origin
     closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  });
+
+  $('.demo').textillate({
+    in: {
+      effect: 'fadeInDownBig'
+    }
+  });
+
+  $('.demoTwo').textillate({
+    initialDelay: 500,
+    in: {
+      effect: 'fadeInUpBig'
+    }
   });
 
 
