ca82ba76ae4e81fdc8ce6d1dd1bdb5388d07247e
Alejandra Jurado <ajurado2204@gmail.com>
2016-06-15 16:13:48 -0400

    modified mySkillsController

--- public/js/controllers/mySkillsController.js
+++ public/js/controllers/mySkillsController.js
@@ -7,6 +7,15 @@ app.controller('mySkillsCtrl', ['$http', '$scope', function($http, $scope){
 
   var msC = $scope;
 
-  msC.topics = 'My Skills!';
+  msC.topic = 'Skills';
+  msC.myResumeLink = 'https://drive.google.com/open?id=0BzP9eaRR3kRiSXBtMmFNQUJDZlE';
+  msC.htmlImage = '../images/html-5.png';
+  msC.javascriptImage = '../images/javascript.png';
+  msC.nodeImage = '../images/nodejs.png';
+  msC.phpImage = '../images/php-logo.png';
+  msC.cssImage = '../images/css-3.png';
+  msC.angularImage = '../images/angular.png';
+  msC.gitImage = '../images/githublogo.png';
+  msC.parallaxBackground = '../images/code.jpg';
 
 }]);
