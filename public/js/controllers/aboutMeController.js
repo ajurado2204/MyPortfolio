ceafb77864a9c798133233f0d386bb9f07c27794
Alejandra Jurado <ajurado2204@gmail.com>
2016-06-15 16:11:51 -0400

    modified aboutMeController and myProjectsController

--- public/js/controllers/aboutMeController.js
+++ public/js/controllers/aboutMeController.js
@@ -5,9 +5,15 @@ var app = angular.module('myPortfolioApp');
 
 app.controller('aboutMeCtrl', ['$http', '$scope', function($http, $scope){
 
-  console.log("Here");
   var amC = $scope;
 
-  amC.topic = 'About Me!';
+  amC.topic = 'About Me';
+  amC.description = "I am a full-stack web developer, driven, self-motivated, curious, and very " +
+    "passionate about web development. I enjoy work that challenges me to learn " +
+    "something new and also strive to better myself as a developer. When I'm " +
+    "not coding I enjoy playing Dragon Age: Inquisition which seems to be an endless " +
+    "game. As well as watching shows online or drawing. Also browsing the web to " +
+    "learn the latest web development techniques in order to provide the best quality in user experience.";
+  amC.parallaxBackground = '../images/art-pencils.jpg';
 
 }]);
--- public/js/controllers/myProjectsController.js
+++ public/js/controllers/myProjectsController.js
@@ -7,6 +7,31 @@ app.controller('myProjectsCtrl', ['$http', '$scope', function($http, $scope){
 
   var mpC = $scope;
 
-  mpC.topics = 'My Projects!';
+  mpC.topic = 'Projects';
+  mpC.githubLink = 'Github Link';
 
+  mpC.liveCatalog = '../images/live-catalog.png';
+  mpC.liveCatalogTitle = 'Live Catalog';
+  mpC.liveCatalogDescription = 'A web application that displays all New York Times Best Sellers books, along with links to Ebay.com and Amazon.com which allows users to buy their favorite books. HTML, CSS, Bootstrap, JavaScript, AJAX, JSON';
+  mpC.liveCatalogLink = 'https://github.com/ajurado2204/8-0project';
+
+  mpC.oneView = '../images/one-view.png';
+  mpC.oneViewTitle = 'One-View';
+  mpC.oneViewDescription = 'A web application that allows users to display content from three different social networks.' +
+    'Displays Slacks incoming messages, Gmail display all unread emails, and for Twitter it ' +
+    'displays the 5 most recent twitter feeds from your home timeline in one single page. HTML, CSS, Materialize, ' +
+    'JavaScript, AngularJS, NodeJS';
+  mpC.oneViewLink = 'https://github.com/ajurado2204/one-messaging';
+
+  mpC.rpsGame = '../images/RockPaperScissors.png';
+  mpC.rpsGameTitle = 'Rock Paper Scissors';
+  mpC.rpsGameDescription = 'Simple Rock Paper Scissors Game.';
+  mpC.rpsGameLink = 'https://github.com/ajurado2204/RockPaperScissor';
+
+  mpC.rutgersFlyer = '../images/rutgersFlyer.png';
+  mpC.rutgersFlyerTitle = 'Rutgers Flyer';
+  mpC.rutgersFlyerDescription = 'A web application that allows users to add locations specifically around Rutgers. It allows logged in users to add locations and insert reviews and ratings for a location. HTML, CSS, Materialize, JavaScript, NodeJS';
+  mpC.rutgersFlyerLink = 'https://github.com/ajurado2204/rutgersflyer';
+
+  mpC.parallaxBackground = '../images/notebook.jpg';
 }]);
