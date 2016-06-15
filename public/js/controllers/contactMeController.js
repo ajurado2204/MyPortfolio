6b362c13d36338ed130e0679e0f5ff9554ba4708
Alejandra Jurado <ajurado2204@gmail.com>
2016-06-15 16:15:51 -0400

    modified contactMeController

--- public/js/controllers/contactMeController.js
+++ public/js/controllers/contactMeController.js
@@ -5,9 +5,13 @@ var app = angular.module('myPortfolioApp');
 
 app.controller('contactMeCtrl', ['$http', '$scope', function($http, $scope){
 
-  console.log("Here");
   var cmC = $scope;
 
-  cmC.topics = 'Contact Me!';
+  cmC.topic = 'Contact Me';
+  cmC.myEmail = 'mailto:alejandra.jurado22@gmail.com';
+  cmC.myLinkedInLink = 'https://www.linkedin.com/in/alejandrajurado';
+  cmC.myLinkedInImage = '../images/webicon-linkedin.png';
+  cmC.myGithubLink = 'https://www.github.com/ajurado2204';
+  cmC.myGithubImage = '../images/GitHub-Mark.png';
 
 }]);
